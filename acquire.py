cd305094e6cab8450f949329bd5a773214607eb9
brandon <brandon.t.navarrete@gmail.com>
2023-02-17 10:38:04 -0600

    revisions and files added

---
+++ acquire.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd 
+import numpy as np
+
+import re
+
+from sklearn.model_selection import train_test_split
+import sklearn.preprocessing
+
+import env
+
+
+# setting connectiong to sequel server using env
+def get_connection(db, user=env.username, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+
+##################
+ 
+# acquiring telco data using a different function
+
+def get_logs_data(get_connection):
+    filename = "logs.csv"
+    
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col = 0)
+    
+    else:
+        
+    # read the SQL query into a dataframe
+        df = pd.read_sql(
+    
+        '''
+        
+       SELECT date,
+       path as endpoint,
+       user_id,
+       cohort_id,
+       ip as source_ip
+        FROM logs;
+        
+        ''', get_connection('curriculum_logs'))
+
+    # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+    # Return the dataframe to the calling code
+    return df 
